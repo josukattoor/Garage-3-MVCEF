105a1810917a64da62ee7e518f4eb1470b773d84
Jossy <josukattoor@gmail.com>
2023-11-20 11:09:27 +0100

    added models

---
+++ Models/ParkingSpot.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage_3_MVCEF.Models
+{
+    public class ParkingSpot
+    {
+        public int Id { get; set; }
+
+        public int ParkingSpotNumber { get; set; }
+    }
+}
---
+++ Models/VehicleType.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Garage_3_MVCEF.Models
+{
+    public class VehicleType
+    {
+        public int Id { get; set; }
+
+        public string VehicleTypeName { get; set; }
+
+        public int ParkingSpot { get; set; }
+
+
+    }
+}
