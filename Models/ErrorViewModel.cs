bcf7eaef27e9f81ac49ded41a9dcc5fbfb134d87
Jossy <josukattoor@gmail.com>
2023-11-20 10:50:20 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Garage_3_MVCEF.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
